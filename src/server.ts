bc89b669b275fad751f47a88cab01a00b5e60de8
bluewarn <n5lp97@gmail.com>
2023-11-05 13:31:10 +0900

    경로 및 포트 수정

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-import passport from './passport/setup.js';
+import passport from './services/passport/setup.js';
 import auth from './routes/auth.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 4444;
+const port = 4443;
 const MONGO_URL = process.env.DB_CONNECTION;
 
 mongoose
