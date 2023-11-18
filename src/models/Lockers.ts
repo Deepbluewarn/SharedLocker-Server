ff1224dc8e4b9944b1c78e80712c4a97133cf9a6
bluewarn <n5lp97@gmail.com>
2023-11-18 20:43:40 +0900

    sharedWith 속성 추가

--- src/models/Lockers.ts
+++ src/models/Lockers.ts
@@ -1,29 +1,28 @@
-import mongoose, { Model, ObjectId, Schema } from 'mongoose';
-import Users from './Users';
+import mongoose, { Model, Schema, Types } from 'mongoose';
 
 const { Types: { ObjectId } } = Schema; // ObjectId 타입은 따로 꺼내주어야 한다.
 
 export interface ILocker extends mongoose.Document {
-    _id: string;
     building: string;
-    floors: {
-        floorNumber: number;
-        lockers: {
-            lockerNumber: number;
-            claimedBy: ObjectId;
-        }[];
-    }[];
+    floors: Floor[];
+}
+
+export interface Locker {
+    lockerNumber: number;
+    claimedBy: Types.ObjectId | null;
+    sharedWith: Types.ObjectId[];
+}
+
+export interface Floor {
+    floorNumber: number;
+    lockers: Locker[];
 }
 
 const LockerSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        required: true,
-        unique: true,
-    },
     building: {
         type: String,
         required: true,
+        unique: true
     },
     floors: [
         {
@@ -39,8 +38,16 @@ const LockerSchema = new mongoose.Schema({
                     },
                     claimedBy: {
                         type: ObjectId,
-                        ref: Users
-                    }
+                        ref: 'Users',
+                        default: null
+                    },
+                    sharedWith: [
+                        {
+                            type: ObjectId,
+                            ref: 'Users',
+                            default: null
+                        }
+                    ]
                 }
             ]
         }
