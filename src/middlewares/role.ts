783c5a46c0c2dbd457d248ba285ee894d14faf70
bluewarn <n5lp97@gmail.com>
2024-05-31 09:21:11 +0000

    메시지 수정

--- src/middlewares/role.ts
+++ src/middlewares/role.ts
@@ -4,6 +4,6 @@ export const checkUserRole = (requiredRoles: string[]) => (req: Request, res: Re
     if (req.admin && requiredRoles.includes(req.admin.role)) {
         return next();
     } else {
-        return res.status(403).json({ success: false, message: 'Access denied. Insufficient permissions.' });
+        return res.status(403).json({ success: false, message: '접근이 제한되었어요. 권한을 확인해주세요.' });
     }
 };
