d3e9845bb4db70a1d328e88a25a9e4d5f10f428b
bluewarn <n5lp97@gmail.com>
2024-10-04 22:35:27 +0900

    보관함 데이터 생성 코드 추가

--- src/db/mongoose_initData.ts
+++ src/db/mongoose_initData.ts
@@ -4,94 +4,61 @@ import Roles from '../models/Roles.js'
 import AdminServices from '../services/admin.services.js'
 import UserService from '../services/user.services.js'
 
-const initialLockers = [
-  {
-    buildingNumber: 23,
-    buildingName: '정보공학관',
-    floors: [
-      {
-        floorNumber: 1,
-        lockers: [
-          {
-            lockerNumber: 101,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 102,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 103,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 104,
-            claimedBy: null,
-            sharedWith: []
-          }
-        ]
-      },
-      {
-        floorNumber: 8,
-        lockers: [
-          {
-            lockerNumber: 101,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 102,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 103,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 104,
-            claimedBy: null,
-            sharedWith: []
-          }
-        ]
-      }
-    ]
-  }, {
-    buildingNumber: 12,
-    buildingName: '중앙도서관',
-    floors: [
-      {
-        floorNumber: 1,
-        lockers: [
-          {
-            lockerNumber: 101,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 102,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 103,
-            claimedBy: null,
-            sharedWith: []
-          },
-          {
-            lockerNumber: 104,
-            claimedBy: null,
-            sharedWith: []
-          }
-        ]
-      }
-    ]
+function getRandomFloorCount() {
+  const floorCounts = [2, 4, 8];
+  return floorCounts[Math.floor(Math.random() * floorCounts.length)];
+}
+
+function generateLockersForFloor(floorNumber: number) {
+  const lockers = [];
+  for (let i = 1; i <= 10; i++) {
+    lockers.push({
+      lockerNumber: i,
+      claimedBy: null,
+      sharedWith: []
+    });
   }
-]
+  return {
+    floorNumber,
+    lockers
+  };
+}
+
+function generateFloors() {
+  const floorCount = getRandomFloorCount();
+  const floors = [];
+  for (let i = 1; i <= floorCount; i++) {
+    floors.push(generateLockersForFloor(i));
+  }
+  return floors;
+}
+
+const buildings = [
+  { buildingNumber: 1, buildingName: '대학본관' },
+  { buildingNumber: 2, buildingName: '법정관' },
+  { buildingNumber: 3, buildingName: '상경관' },
+  { buildingNumber: 5, buildingName: '국제관' },
+  { buildingNumber: 7, buildingName: '상영관(제2학생회관)' },
+  { buildingNumber: 8, buildingName: '수덕전(학생회관)' },
+  { buildingNumber: 9, buildingName: '제1인문관' },
+  { buildingNumber: 10, buildingName: '제2인문관' },
+  { buildingNumber: 12, buildingName: '중앙도서관' },
+  { buildingNumber: 15, buildingName: '의료보건관' },
+  { buildingNumber: 16, buildingName: '생활과학관' },
+  { buildingNumber: 17, buildingName: '음악관' },
+  { buildingNumber: 18, buildingName: '창의관' },
+  { buildingNumber: 19, buildingName: '지천관' },
+  { buildingNumber: 20, buildingName: '산학협력관' },
+  { buildingNumber: 21, buildingName: '건윤관' },
+  { buildingNumber: 22, buildingName: '공학관' },
+  { buildingNumber: 23, buildingName: '정보공학관' }
+];
+
+const initialLockers = buildings.map(building => ({
+  buildingNumber: building.buildingNumber,
+  buildingName: building.buildingName,
+  floors: generateFloors()
+}));
 
 const initialRoles = [
   {
