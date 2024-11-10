31d7e6fda9d18402f9dd232c4e423e83267ce259
bluewarn <n5lp97@gmail.com>
2024-11-10 02:21:37 +0000

    express payload 제한 수정

--- src/server.ts
+++ src/server.ts
@@ -32,8 +32,8 @@ mongoose
 app.use(PinoHttp({logger: logger}))
 app.use(cors())
 app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: false }))
 app.use(passport.initialize())
 
 app.use('/auth', auth)
